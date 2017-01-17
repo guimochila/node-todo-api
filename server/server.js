238aad307454cb606f44f354d10c25183c543f46
Guilherme <gui.mochila@gmail.com>
2017-01-17 19:30:30 +0000

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todos');
@@ -27,11 +28,30 @@ app.post('/todos', (req,res) => {
 /* 
   GET /todos
 */
-
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) =>{
     res.send({todos})
   }).catch(e => res.status(400).send(e));
+});
+
+/*
+  GET /todos/:id
+*/
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then((todo) => {
+      if(!todo) {
+        return res.status(404).send();
+      }
+      
+      res.send({ todo });
+    }).catch(e => res.status(400).send());
 });
 
 app.listen(3000, () => {
