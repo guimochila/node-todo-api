99001ee298307dc9b23b341ac60d36e40a947562
Guilherme <gui.mochila@gmail.com>
2019-04-26 19:12:38 +0200

    📝 Adding comments

--- src/handlers/errorHandler.js
+++ src/handlers/errorHandler.js
@@ -1,3 +1,7 @@
+/* 
+  Instead of using try{} catch(e) {} in each controller, we wrap the function in
+  catchErrors(), catch any errors they throw, and pass it along to our express middleware with next() 
+*/
 export const catchErrors = fn => {
   return function handleError(req, res, next) {
     return fn(req, res, next).catch(next);
