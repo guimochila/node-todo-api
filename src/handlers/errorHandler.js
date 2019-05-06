059c690c0499af0e7ffd5ee8f4bd9a61ba266bab
Guilherme <gui.mochila@gmail.com>
2019-05-06 21:38:36 +0200

    ♻️ Error handler for production added.

--- src/handlers/errorHandler.js
+++ src/handlers/errorHandler.js
@@ -1,20 +1,12 @@
-/* 
-  Instead of using try{} catch(e) {} in each controller, we wrap the function in
-  catchErrors(), catch any errors they throw, and pass it along to our express middleware with next() 
-*/
-export const catchErrors = fn => {
-  return function handleError(req, res, next) {
-    return fn(req, res, next).catch(next);
-  };
-};
-
 export const developmentErrors = (err, req, res, next) => {
   const stack = err.stack || '';
-  const errorDetails = {
+  const error = {
     message: err.message,
-    status: err.status,
     stack,
   };
-  res.status(err.status || 500);
-  res.json(errorDetails);
+  return res.status(500).json({ data: { error } });
+};
+
+export const productionErrros = (err, req, res, next) => {
+  return res.status(500).json({ data: { error: err.message } });
 };
