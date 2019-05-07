0064dfbc6a965f66aca7b18fc2fc322720490b67
Guilherme <gui.mochila@gmail.com>
2019-05-07 19:39:34 +0200

    ♻️ Removed try/catch. Redundant.

--- src/services/todo.services.js
+++ src/services/todo.services.js
@@ -3,58 +3,37 @@ import { model } from 'mongoose';
 const Todo = model('Todo');
 
 const createTodo = async todo => {
-  try {
-    const newTodo = await new Todo(todo).save();
-    return newTodo;
-  } catch (e) {
-    console.log(e);
-    return e;
-  }
+  const newTodo = await new Todo(todo).save();
+  return newTodo;
 };
 
 const findTodos = async () => {
-  try {
-    const todos = await Todo.find({});
-    return todos;
-  } catch (e) {
-    console.log(e);
-  }
+  const todos = await Todo.find({});
+  return todos;
 };
 
 const findOneTodo = async id => {
-  try {
-    const todo = await Todo.findOne({ _id: id });
-    return todo;
-  } catch (e) {
-    console.log(e);
-  }
+  const todo = await Todo.findOne({ _id: id });
+  return todo;
 };
 
 const updateTodo = async (id, update) => {
-  try {
-    const todo = await Todo.findByIdAndUpdate(
-      { _id: id },
-      {
-        ...update,
-      },
-      {
-        new: true,
-        runValidators: true,
-      },
-    );
-    return todo;
-  } catch (e) {
-    console.log(e);
-  }
+  const todo = await Todo.findByIdAndUpdate(
+    { _id: id },
+    {
+      ...update,
+    },
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+  return todo;
 };
 
 const removeTodo = async id => {
-  try {
-    const todo = await Todo.findByIdAndDelete({ _id: id });
-    return todo;
-  } catch (e) {
-    console.log(e);
-  }
+  const todo = await Todo.findByIdAndDelete({ _id: id });
+  return todo;
 };
 
 export default {
